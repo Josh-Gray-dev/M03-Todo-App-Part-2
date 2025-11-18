ee0e5c75298b4d002f3cd31a1ba76b0948ad4450
Josh Gray <claypool7@gmail.com>
2025-11-17 19:19:26 -0500

    added write a task and +

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Tasks';
 
 export default function App() {
@@ -16,6 +16,20 @@ export default function App() {
           <Task text={'Task 2'} />
         </View>
 
+        {/* Write a task*/}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+
+          <TextInput style={styles.input} placeholder={'Write a task'} />
+
+          <TouchableOpacity >
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
 
       </View>
     </View>
@@ -38,4 +52,16 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+    
+  },
+  input: {},
+  addWrapper: {},
+  addText: {},
 });
